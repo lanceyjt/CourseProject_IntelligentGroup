b0af155b1ce9e812fdbdac3f76ad9097c68baa1d
Jingtian Yao <jy2867@columbia.edu>
2022-11-20 15:10:25 -0800

    Make log more readable

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ def main():
         print("------- Analysis for Query: {} -------".format(query))
         sa = search_action.SearchAction(query, 30)
 
+        print("==== Web Scraper ====")
         print("Invoke url iterator...")
         sa.invoke_url_iterator(num_extra_doc=8)
 
@@ -18,19 +19,28 @@ def main():
         sa.build_corpus()
 
         corpus = sa.corpus
-        print(len(corpus))
+        print("Length of corpus: ", len(corpus))
+        print("====================")
+        print("")
 
         with open("./output/{q}_corpus.txt".format(q=query), "w") as file:
             file.write(str(corpus))
 
+        print("==== BM25 Ranking ====")
         res = sa.get_bm25_scores()
         print("Docs with BM25 scores in descending order: ")
-        print(res)
-
+        for d in res:
+            print(d)
+        print("======================")
+        print("")
+        
+        print("==== LDA Topic Modeling ====")
         topics = sa.lda_topic_model(num_topic=5, k=10)
         for i in range(len(topics)):
             print("Topic {i} for query '{q}': ".format(i=i+1, q=query))
             print(topics[i])
+        print("============================")
+        print("")
 
 if __name__ == '__main__':
     main()
