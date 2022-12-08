78f2b8c6fc1b19cc2232b39f721959880a422425
Jingtian Yao <jy2867@columbia.edu>
2022-12-08 02:15:26 -0500

    Beautify the HTTP response

--- mysite/intelligentgroup/views.py
+++ mysite/intelligentgroup/views.py
@@ -35,13 +35,39 @@ def search_action_analysis(request):
 
     topics = sa.lda_topic_model(num_topic=5, k=10)
 
-    data = {
-        'query_input': query,
-        'bm25_scores': bm25_scores,
-        'word_freq': word_freq_to_return,
-        'topics': topics
-    }
-    
-    print('json-data to be sent: ', data)
+    #data = {
+    #    'query_input': query,
+    #    'bm25_scores': bm25_scores,
+    #    'word_freq': word_freq_to_return,
+    #    'topics': topics
+    #}
 
-    return JsonResponse(data)
+    response = HttpResponse()
+    response.write("<h3>Query Input</h3>") 
+    response.write("<p>%s</p>" % query)
+
+    response.write("<h3>Web Documents: bm25 scores & (word,frequency) pair</h3>")
+    for r in range(len(bm25_scores)):
+        word_freq = word_freq_to_return[bm25_scores[r][0]]
+        word_freq_str = "; ".join(list(map(lambda x: "("+x+")", word_freq)))
+        response.write("<p>--------------<br>")
+        response.write("<span class=\"bolded\">URL: %s</span><br>" % bm25_scores[r][0])
+        response.write("Score: %s<br>" % bm25_scores[r][1])
+        response.write("Rank: %s<br>" % str(r+1))
+        response.write("Top 5 Word Frequency:<br>")
+        response.write("    %s<br>" % word_freq_str)
+        response.write("</p>")
+
+    response.write("<h3>Topics: represented by top 10 words</h3>")
+    for t in range(len(topics)):
+        topic_str = "(" + ", ".join(topics[t]) + ")"
+        response.write("<p>--------------<br>")
+        response.write("<span class=\"bolded\">Topic #%s</span><br>" % str(t+1))
+        response.write("     %s<br>" % topic_str)
+        response.write("</p>")
+    
+
+    # print('json-data to be sent: ', data)
+
+    # return JsonResponse(data)
+    return response
